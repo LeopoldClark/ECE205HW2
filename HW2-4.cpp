342a24bfe7f72934819692c6763cccdde0a422d7
LeopoldClark <lkc227@lehigh.edu>
2026-02-11 19:59:02 -0500

    Add files via upload

--- HW2-4.cpp
+++ HW2-4.cpp
@@ -1,17 +1,18 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> //includes input output stream library
+#include <cmath> //includes cmath library
 
-using namespace std;
+using namespace std; //uses namespace std
 
-int main(){
+int main(){ //initiates main function
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 4; i++){ //for loop beginning at 0 and iterating 4 times for the 4 chosen
         
-        srand(time(0) + i);
-        int number;
-        number = rand() % 25 + 1;
+        srand(time(0) + i); //chooses random seed based on time ensuring no repeating results and the 
+        //i is added to ensure a different number otherwise the same number would be output each iteration
+        int number; //assigns number variable to store the value
+        number = rand() % 25 + 1; //value randomly chosen between 1 and 25 with the random function
         
-        cout << "finalist " << number << " is a winner" << endl;
+        cout << "finalist " << number << " is a winner" << endl; //output statement
     }
 
     return 0;
